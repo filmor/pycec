1b8c57a217bb4f78dfd3d5d178b76841e85c77a2
Benedikt Sauer <filmor@gmail.com>
2013-11-30 16:32:36 +0100

    Catch exceptions in callbacks.

--- cec_callbacks.pyx
+++ cec_callbacks.pyx
@@ -8,43 +8,49 @@ cdef int log_message_cb(void* context, cec_log_message msg):
     try:
         self = <object>context
         self.log_callback(msg.level, msg.message)
-    finally:
-        return 1
+    except:
+        pass
+    return 1
 
 cdef int key_press_cb(void* context, cec_keypress msg):
     try:
         self = <object>context
         self.key_callback()
-    finally:
-        return 1
+    except:
+        pass
+    return 1
 
 cdef int command_cb(void* context, cec_command msg):
     try:
         self = <object>context
         self.command_callback()
-    finally:
-        return 1
+    except:
+        pass
+    return 1
 
 cdef int config_cb(void* context, const libcec_configuration msg):
     try:
         self = <object>context
         self.config_callback()
-    finally:
-        return 1
+    except:
+        pass
+    return 1
 
 cdef int alert_cb(void* context, const libcec_alert msg1, const libcec_parameter msg2):
     try:
         self = <object>context
         self.alert_callback()
-    finally:
-        return 1
+    except:
+        pass
+    return 1
 
 cdef int menu_cb(void* context, const cec_menu_state msg):
     try:
         self = <object>context
         self.menu_callback()
-    finally:
-        return 1
+    except:
+        pass
+    return 1
 
 cdef void source_cb(void* context, const cec_logical_address msg1, const uint8_t msg2):
     try:
